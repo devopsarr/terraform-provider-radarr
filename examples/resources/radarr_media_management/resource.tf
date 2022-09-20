fa458adc50a0cfd0a3206e4aa25c641f79d1e50c
Fuochi <ffuochi@hotmail.com>
2022-09-20 19:10:03 +0400

    feat: add media_management resource

---
+++ examples/resources/radarr_media_management/resource.tf
@@ -0,0 +1,9 @@
+resource "radarr_media_management" "example" {
+  include_quality            = false
+  rename_movies              = true
+  replace_illegal_characters = false
+  replace_spaces             = false
+  colon_replacement_format   = "%s"
+  standard_movie_format      = "{Movie Title} ({Release Year}) {Quality Full}"
+  movie_folder_format        = "{Movie Title} ({Release Year})"
+}
---
+++ internal/provider/media_management_resource_test.go
@@ -0,0 +1,66 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccMediaManagementResource(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccMediaManagementResourceConfig("none"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("radarr_media_management.test", "file_date", "none"),
+					resource.TestCheckResourceAttrSet("radarr_media_management.test", "id"),
+				),
+			},
+			// Update and Read testing
+			{
+				Config: testAccMediaManagementResourceConfig("cinemas"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("radarr_media_management.test", "file_date", "cinemas"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "radarr_media_management.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccMediaManagementResourceConfig(date string) string {
+	return fmt.Sprintf(`
+	resource "radarr_media_management" "test" {
+		auto_unmonitor_previously_downloaded_movies = false
+		recycle_bin = ""
+		recycle_bin_cleanup_days = 7
+		download_propers_and_repacks = "doNotPrefer"
+		create_empty_movie_folders = false
+		delete_empty_folders = false
+		file_date = "%s"
+		rescan_after_refresh = "afterManual"
+		auto_rename_folders = false
+		paths_default_static = false
+		set_permissions_linux = false
+		chmod_folder = 755
+		chown_group = ""
+		skip_free_space_check_when_importing = false
+		minimum_free_space_when_importing = 100
+		copy_using_hardlinks = true
+		import_extra_files = true
+		extra_file_extensions = "srt"
+		enable_media_info = true
+	}`, date)
+}
