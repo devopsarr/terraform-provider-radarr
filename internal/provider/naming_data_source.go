7c2aa8a130d4cb877f890b5d2e06a243668f3f47
Fuochi <ffuochi@hotmail.com>
2022-09-20 19:11:49 +0400

    feat: add naming datasource

---
+++ internal/provider/naming_data_source.go
@@ -0,0 +1,112 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"golift.io/starr/radarr"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var _ datasource.DataSource = &NamingDataSource{}
+
+func NewNamingDataSource() datasource.DataSource {
+	return &NamingDataSource{}
+}
+
+// NamingDataSource defines the naming implementation.
+type NamingDataSource struct {
+	client *radarr.Radarr
+}
+
+func (d *NamingDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_naming"
+}
+
+func (d *NamingDataSource) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		// This description is used by the documentation generator and the delay server.
+		MarkdownDescription: "[subcategory:Media Management]: #\n[Naming](../resources/naming).",
+		Attributes: map[string]tfsdk.Attribute{
+			"id": {
+				MarkdownDescription: "Delay Profile ID.",
+				Computed:            true,
+				Type:                types.Int64Type,
+			},
+			"include_quality": {
+				MarkdownDescription: "Include quality in file name.",
+				Computed:            true,
+				Type:                types.BoolType,
+			},
+			"rename_movies": {
+				MarkdownDescription: "Radarr will use the existing file name if false.",
+				Computed:            true,
+				Type:                types.BoolType,
+			},
+			"replace_illegal_characters": {
+				MarkdownDescription: "Replace illegal characters. They will be removed if false.",
+				Computed:            true,
+				Type:                types.BoolType,
+			},
+			"replace_spaces": {
+				MarkdownDescription: "Replace spaces.",
+				Computed:            true,
+				Type:                types.BoolType,
+			},
+			"colon_replacement_format": {
+				MarkdownDescription: "Change how Radarr handles colon replacement. Valid values are: 'delete', 'dash', 'spaceDash', and 'spaceDashSpace'.",
+				Computed:            true,
+				Type:                types.StringType,
+			},
+			"movie_folder_format": {
+				MarkdownDescription: "Movie folder format.",
+				Computed:            true,
+				Type:                types.StringType,
+			},
+			"standard_movie_format": {
+				MarkdownDescription: "Standard movie formatss.",
+				Computed:            true,
+				Type:                types.StringType,
+			},
+		},
+	}, nil
+}
+
+func (d *NamingDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*radarr.Radarr)
+	if !ok {
+		resp.Diagnostics.AddError(
+			UnexpectedDataSourceConfigureType,
+			fmt.Sprintf("Expected *radarr.Radarr, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.client = client
+}
+
+func (d *NamingDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	// Get naming current value
+	response, err := d.client.GetNamingContext(ctx)
+	if err != nil {
+		resp.Diagnostics.AddError(ClientError, fmt.Sprintf("Unable to read naming, got error: %s", err))
+
+		return
+	}
+
+	tflog.Trace(ctx, "read naming")
+
+	result := writeNaming(response)
+	resp.Diagnostics.Append(resp.State.Set(ctx, &result)...)
+}
