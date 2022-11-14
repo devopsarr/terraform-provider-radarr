f59290c69d9a9483eded499a5c94c3f80212abf5
Fuochi <ffuochi@hotmail.com>
2022-11-14 14:09:24 +0400

    feat: add notification resource

---
+++ examples/resources/radarr_notification/resource.tf
@@ -0,0 +1,20 @@
+resource "radarr_notification" "example" {
+  on_grab                          = false
+  on_download                      = true
+  on_upgrade                       = true
+  on_rename                        = false
+  on_movie_added                   = false
+  on_movie_delete                  = false
+  on_movie_file_delete             = false
+  on_movie_file_delete_for_upgrade = true
+  on_health_issue                  = false
+  on_application_update            = false
+
+  include_health_warnings = false
+  name                    = "Example"
+
+  implementation  = "CustomScript"
+  config_contract = "CustomScriptSettings"
+
+  path = "/scripts/radarr.sh"
+}
