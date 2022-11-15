b5e20e6d0415dcc87ece3a0a395d154b0e5173ce
Fuochi <ffuochi@hotmail.com>
2022-11-15 11:49:28 +0400

    feat: add indexer newznab resource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -135,6 +135,7 @@ func (p *RadarrProvider) Resources(ctx context.Context) []func() resource.Resour
 		NewDelayProfileResource,
 		NewIndexerConfigResource,
 		NewIndexerResource,
+		NewIndexerNewznabResource,
 		NewMediaManagementResource,
 		NewNamingResource,
 		NewNotificationResource,
