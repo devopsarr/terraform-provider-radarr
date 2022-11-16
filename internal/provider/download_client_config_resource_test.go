891e8d9b01f9b313f5666bbe350c7e006cedcadb
Fuochi <ffuochi@hotmail.com>
2022-11-16 15:51:19 +0400

    feat: add download client config resource

---
+++ internal/provider/download_client_config_resource_test.go
@@ -0,0 +1,50 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccDownloadClientConfigResource(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccDownloadClientConfigResourceConfig("true"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("radarr_download_client_config.test", "auto_redownload_failed", "true"),
+					resource.TestCheckResourceAttrSet("radarr_download_client_config.test", "id"),
+				),
+			},
+			// Update and Read testing
+			{
+				Config: testAccDownloadClientConfigResourceConfig("false"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("radarr_download_client_config.test", "auto_redownload_failed", "false"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "radarr_download_client_config.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccDownloadClientConfigResourceConfig(redownload string) string {
+	return fmt.Sprintf(`
+	resource "radarr_download_client_config" "test" {
+		check_for_finished_download_interval = 1
+		enable_completed_download_handling = true
+		auto_redownload_failed = %s
+	}`, redownload)
+}
