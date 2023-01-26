b0c06a03a770d2a1901d384d534a90c51d8e8079
Fuochi <ffuochi@hotmail.com>
2023-01-26 17:14:37 +0400

    feat: add metadata roksbox resource

---
+++ examples/resources/radarr_metadata_roksbox/import.sh
@@ -0,0 +1,2 @@
+# import using the API/UI ID
+terraform import radarr_metadata_roksbox.example 1
---
+++ internal/provider/metadata_roksbox_resource_test.go
@@ -0,0 +1,51 @@
+package provider
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccMetadataRoksboxResource(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccMetadataRoksboxResourceConfig("roksboxResourceTest", "false"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("radarr_metadata_roksbox.test", "movie_metadata", "false"),
+					resource.TestCheckResourceAttrSet("radarr_metadata_roksbox.test", "id"),
+				),
+			},
+			// Update and Read testing
+			{
+				Config: testAccMetadataRoksboxResourceConfig("roksboxResourceTest", "true"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("radarr_metadata_roksbox.test", "movie_metadata", "true"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "radarr_metadata_roksbox.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccMetadataRoksboxResourceConfig(name, metadata string) string {
+	return fmt.Sprintf(`
+	resource "radarr_metadata_roksbox" "test" {
+		enable = false
+		name = "%s"
+		movie_metadata = %s
+		movie_images = true
+	}`, name, metadata)
+}
